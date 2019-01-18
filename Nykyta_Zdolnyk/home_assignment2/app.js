624b5a4554940bbaa7d1e2521962906a3d83b118
Nykyta Zdolnyk <zdolniknikita@gmail.com>
2019-01-18 17:33:35 +0200

    Added -- in the end

--- Nykyta_Zdolnyk/home_assignment2/app.js
+++ Nykyta_Zdolnyk/home_assignment2/app.js
@@ -1,6 +1,6 @@
 var a = prompt("Введите первое число");
 var b = prompt("Введите второе число");
 
-document.write("-- Произведение чисел = " + (a * b) + "<br>" );
-document.write("-- Деление чисел = " + (a / b));
+document.write ("-- Произведение чисел = " + (a * b) + " --" + "<br>");
+document.write ("-- Деление чисел = " + (a / b) + " --");
 
