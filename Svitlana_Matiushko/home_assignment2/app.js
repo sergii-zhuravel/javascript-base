7ce0f5338f35abb0efb26cef4ae6f1ea828a14c1
Svitlana Matiushko <matiushko@gmail.com>
2019-01-19 14:36:33 +0200

    Home task №2

--- Svitlana_Matiushko/home_assignment2/app.js
+++ Svitlana_Matiushko/home_assignment2/app.js
@@ -3,5 +3,6 @@ var b = prompt('Type second number' ,);
 var c = a * b;
 var d = b / a;
 
-document.write( c + "<br>"); 
+document.write( c + "<br>"); // first version for teg <br>
+document.write("<br>"); //second version for teg <br>
 document.write( d );
