827023a8810074bd84c0985e6bd51314d47468cc
Lena Loshakova <you@example.com>
2019-01-20 17:09:03 +0200

    home_assignment2

---
+++ lena_loshakova/home_assignment2/app.js
@@ -0,0 +1,5 @@
+var a = prompt("Enter the first number");
+var b = prompt("Enter the second number");
+
+document.write("-- Произведение чисел =" + (a * b) + "--");
+document.write("-- Деление чисел =" + (a / b) + "--");
