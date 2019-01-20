22aa671e9f4a6df170046599c903a2b9d1b9046b
Lena Loshakova <you@example.com>
2019-01-20 17:25:52 +0200

    home_assignment2

--- lena_loshakova/home_assignment2/app.js
+++ lena_loshakova/home_assignment2/app.js
@@ -1,5 +1,5 @@
 var a = prompt("Enter the first number");
 var b = prompt("Enter the second number");
 
-document.write("-- Произведение чисел =" + (a * b) + "--");
-document.write("-- Деление чисел =" + (a / b) + "--");
+document.write("-- Произведение чисел = " + (a * b) + " --" + "<br>");
+document.write("-- Деление чисел = " + (a / b) + " --");
