640c58aee15ca8fc43d5ace0b36d315d5f77d717
Svitlana Matiushko <matiushko@gmail.com>
2019-01-21 18:52:32 +0200

    added text

--- Svitlana_Matiushko/home_assignment2/app.js
+++ Svitlana_Matiushko/home_assignment2/app.js
@@ -2,7 +2,6 @@ var a = prompt('Type first number' ,);
 var b = prompt('Type second number' ,);
 var c = a * b;
 var d = b / a;
-
-document.write( c + "<br>"); // first version for teg <br>
-document.write("<br>"); //second version for teg <br>
-document.write( d );
+document.write("Произведение чисел = " + c); 
+document.write("<br>"); 
+document.write("Деление чисел = " + d );
