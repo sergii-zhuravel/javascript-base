7af67037fbadca0d7a8fce160386adc727747df6
Svitlana Matiushko <matiushko@gmail.com>
2019-01-22 11:15:24 +0200

    added "--" in text

--- Svitlana_Matiushko/home_assignment2/app.js
+++ Svitlana_Matiushko/home_assignment2/app.js
@@ -1,7 +1,7 @@
-var a = prompt('Type first number' ,);
-var b = prompt('Type second number' ,);
+var a = prompt("Type first number");
+var b = prompt("Type second number");
 var c = a * b;
 var d = b / a;
-document.write("Произведение чисел = " + c); 
-document.write("<br>"); 
-document.write("Деление чисел = " + d);
+document.write("-- Произведение чисел = " + c + "\n" + "--");
+document.write("<br>");
+document.write("-- Деление чисел = " + d + "\n" + "--");
