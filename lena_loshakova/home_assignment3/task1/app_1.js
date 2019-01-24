215cf8ea14a0e2ccc9f15d6a58ad1f280e172037
Lena Loshakova <you@example.com>
2019-01-24 21:11:34 +0200

    home task

--- lena_loshakova/home_assignment3/task1/app_1.js
+++ lena_loshakova/home_assignment3/task1/app_1.js
@@ -1,8 +1,6 @@
-//task 1
 var a = prompt("Enter the number:");
 document.write("<pre>");
 
-document.writeln("ЗАДАЧА 1:" );
 document.writeln("Ввод: " + a );
 
  a++;
@@ -13,8 +11,3 @@ document.writeln("Ввод: " + a );
 
  document.writeln("Результат: " + a ); 
 
- //task 2
- var b = prompt("Enter the fractional number:");
-
- document.writeln("ЗАДАЧА 2:" );
- document.writeln("Результат: " + ~~b );
