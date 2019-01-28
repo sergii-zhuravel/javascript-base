6b142092d061fd13137de7a83f035089b29ad4b6
KorostelevMihail <38920401+KorostelevMihail@users.noreply.github.com>
2019-01-28 16:02:36 +0200

    Home Work 4 task 1

--- Korostelev_Mihail/home_assignment4/taskOne/app/app.js
+++ Korostelev_Mihail/home_assignment4/taskOne/app/app.js
@@ -1,2 +1,7 @@
 var userValue = Number(prompt("Enter your value"));
-console.log(userValue);
+var numberValue = 1;
+do {
+   numberValue++;
+  if (numberValue % 2 == 1) continue;
+  document.write(numberValue + '<br />');
+} while (numberValue < userValue);
