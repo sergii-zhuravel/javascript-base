186486fbcb68e9d388ce4849d1a9eac747d43b68
Svitlana Matiushko <matiushko@gmail.com>
2019-01-28 19:31:06 +0200

    home work 4

---
+++ Svitlana_Matiushko/home_assignment4/Task_1/app.js
@@ -0,0 +1,7 @@
+var a = prompt("Введите число");
+
+for (i = 1; i <= a; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
---
+++ Svitlana_Matiushko/home_assignment4/Task_2_variant_1/app.js
@@ -0,0 +1,8 @@
+do {
+  var i = prompt("Type number");
+  if (i % 2 == 0) {
+    console.log("Even number");
+  } else {
+    console.log("Odd number");
+  }
+} while (i != 0);
