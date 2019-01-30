8f720e343e6ffc6b283acb1bbe54dfdfbbcf2b5b
Lena Loshakova <you@example.com>
2019-01-30 21:10:22 +0200

    ht №4

--- lena_loshakova/home_assignment4/task2/app2.js
+++ lena_loshakova/home_assignment4/task2/app2.js
@@ -1,28 +1,14 @@
-/*while (true) {
-    var userNum = parseInt(prompt("Введите число и нажмите 'Ok'! (для выхода введите '0')"));
-    if (!userNum) {
-        break;
-    }
-    if (userNum % 2) {
-        document.write("Число " + userNum + " является нечетным" + "<br>");
-      
-    } else {
-        document.write("Число " + userNum + " является четным" + "<br>");
-     
-    }
-}*/
 do {
   var number = prompt("Enter the number :");
 
-  if (number % 2 === 0) {
-    //if(number === 0){break;}
+  if (number % 2 == 0) {
+    if (number == 0) {
+      break;
+    }
     document.write("Четное число: " + number + "</br>");
   } else {
-    if(number === 0){break;}
     document.write("Нечетное число: " + number + "</br>");
-
   }
-  
 } while (number != 0);
 {
   document.write(" Вы ввели  '0' ");
