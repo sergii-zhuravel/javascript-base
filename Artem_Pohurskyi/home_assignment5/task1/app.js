c1cf2e0cdbb87adacdbea0146b310367c83d595f
Pohurskyi Artem <joker95282@gmail.com>
2019-02-02 22:05:36 +0200

    home_assignment5

--- Artem_Pohurskyi/home_assignment5/task1/app.js
+++ Artem_Pohurskyi/home_assignment5/task1/app.js
@@ -1,11 +1,6 @@
 var a = parseInt(prompt('Введите число "a":'));
 var b = parseInt(prompt('Введите число "b":'));
-var Num = parseInt(
-  prompt(
-    "Введите номер математической операции (1 - Сумма; 2 - Разница; 3 - Произведение; 4 - Частное):"
-  )
-);
-function sum(a,b) {
+function sum(a, b) {
   return a + b;
 }
 function sub(a, b) {
@@ -17,30 +12,33 @@ function mult(a, b) {
 function div(a, b) {
   return a / b;
 }
-
-  if (Num <= 4 & Num > 0) {
-    switch (Num) {
+var num = parseInt(
+  prompt(
+    "Введите номер математической операции (1 - Сумма; 2 - Разница; 3 - Произведение; 4 - Частное):"
+  )
+);
+for (var i = 1; i > 0; i++) {
+  if ((num > 0) & (num < 5)) {
+    switch (num) {
       case 1:
         console.log("Результат операции (сумма):" + sum(a, b));
         break;
       case 2:
-        console.log("Результат операции (разница):" + sub(a,b));
+        console.log("Результат операции (разница):" + sub(a, b));
         break;
       case 3:
-        console.log("Результат операции (умножение):" + mult(a,b));
+        console.log("Результат операции (умножение):" + mult(a, b));
         break;
       case 4:
-        console.log("Результат операции (частное):" + div(a,b));
-        break;
-      default:
+        console.log("Результат операции (частное):" + div(a, b));
         break;
     }
+    break;
   } else {
-    for (var i = 0; i < 1000000; i++) {
-    var Num = parseInt(
+    num = parseInt(
       prompt(
         "Вы ввели не правильный номер! Введите номер математической операции (1 - Сумма; 2 - Разница; 3 - Произведение; 4 - Частное):"
       )
     );
-  } 
+  }
 }
--- Artem_Pohurskyi/home_assignment5/task2/app.js
+++ Artem_Pohurskyi/home_assignment5/task2/app.js
@@ -1,13 +1,17 @@
-var Num = parseInt(prompt('Введите число. Для выхода введите "0"'));
+var num = parseInt(prompt('Введите число. Для выхода введите "0"'));
 
-function checkNum(Num){
-    if(Num % 2 == 1){
-        return 'Вы ввели: ' + Num + '.Это не чётное число';
-    }
-    else{
-        return 'Вы ввели: ' + Num + '.Это чётное число';
-    } 
+function checkNum(num) {
+  if (num % 2 == 1) {
+    return "Вы ввели: " + num + ".Это не чётное число";
+  } else {
+    return "Вы ввели: " + num + ".Это чётное число";
+  }
 }
-while(Num != 0){
-        console.log(checkNum(Num));
+for (var i = 1; i > 0; i++) {
+  if (num != 0) {
+    console.log(checkNum(num));
+    num = parseInt(prompt('Введите число. Для выхода введите "0"'));
+  } else {
+    break;
+  }
 }
